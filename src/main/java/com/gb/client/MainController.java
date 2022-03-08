23bca7ec3c6299dccdbf62752c21293d87775d9a
manoffna <2780nm@gmail.com>
2022-03-08 23:53:15 +0300

    GB -> File Storage -> Lesson 1

--- src/main/java/com/gb/client/MainController.java
+++ src/main/java/com/gb/client/MainController.java
@@ -1,5 +1,6 @@
 package com.gb.client;
 
+import com.sun.javafx.application.PlatformImpl;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
@@ -12,23 +13,32 @@ import java.net.Socket;
 import java.net.URL;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import static java.lang.String.valueOf;
 
 public class MainController implements Initializable {
     private static final int BUFFER_SIZE = 256;
-
     private File currentDirectory;
+   //private File serverUserDirectory;
     public TextField clientPath;
     public TextField serverPath;
-    public ComboBox<String> disks;
+    public ComboBox <String> disks;
     public ListView<String> clientView;
+
+    // данных нет на клиенте
     public ListView<String> serverView;
+    //private File[] rootDrive = File.listRoots();
 
     private DataInputStream is;
     private DataOutputStream os;
     private byte[] buf;
 
     // Platform.runlater(() -> {})
+
     private void updateClientView() {
         Platform.runLater(() -> {
             clientPath.setText(currentDirectory.getAbsolutePath());
@@ -36,7 +46,18 @@ public class MainController implements Initializable {
             clientView.getItems().add("...");
             clientView.getItems()
                     .addAll(currentDirectory.list());
-            //disks.getItems().add(String e);
+            //disks.getItems()
+                    //.add(String.valueOf(rootDrive));
+        });
+    }
+
+    private void updateServerView(List<String> names ) {
+        Platform.runLater(() -> {
+            //serverPath.setText(serverUserDirectory.getAbsolutePath());
+            serverView.getItems().clear();
+            serverView.getItems().add("...");
+            serverView.getItems()
+                    .addAll(names);
         });
     }
 
@@ -55,12 +76,55 @@ public class MainController implements Initializable {
                 }
             }
             os.flush();
-
         }
     }
 
-    public void download(ActionEvent actionEvent) {
 
+    //Download file from server
+    public void download(ActionEvent actionEvent) throws IOException {
+        os.writeUTF("#get_file#");
+        os.writeUTF(serverView.getSelectionModel().getSelectedItem());
+        os.flush();
+    }
+
+    private void readFromInputStream() {
+        try {
+            while (true) {
+                String command = is.readUTF();
+                //#list# size name1 name2 .... name_size
+                //#path# pathName
+                //#path_action# dirName
+                if ("#list#".equals(command)) {
+                    List<String> names = new ArrayList<>();
+                    int count = is.readInt();
+                    for (int i = 1; i < count; i++) {
+                        String name = is.readUTF();
+                        names.add(name);
+                    }
+                    updateServerView(names);
+                }
+                if ("#path#".equals(command)) {
+                    String serverDir = is.readUTF();
+                    Platform.runLater(() -> serverPath.setText(serverDir));
+                }
+                if ("#file_message".equals(command)) {
+                    String name = is.readUTF();
+                    long size = is.readLong();
+                    File newFile = currentDirectory.toPath()
+                            .resolve(name)
+                            .toFile();
+                    try (OutputStream fos = new FileOutputStream(newFile)) {
+                        for (int i = 0; i < (size + BUFFER_SIZE - 1) / BUFFER_SIZE; i++) {
+                            int readCount = is.read(buf);
+                            fos.write(buf, 0, readCount);
+                        }
+                    }
+                    updateClientView();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void initNetwork() {
@@ -69,6 +133,9 @@ public class MainController implements Initializable {
             Socket socket = new Socket ("localhost", 8189);
             is = new DataInputStream(socket.getInputStream());
             os = new DataOutputStream(socket.getOutputStream());
+            Thread readThread = new Thread(this::readFromInputStream);
+            readThread.setDaemon(true);
+            readThread.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -77,11 +144,13 @@ public class MainController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         currentDirectory = new File (System.getProperty("user.home"));
+        //serverUserDirectory = new File("C:\\Users\\nmanov\\IdeaProjects\\gb\\feb-2022\\cloud-storage\\server");
         FileSystems.getDefault().getFileStores().forEach(
                 f -> System.out.println(f.name())
         );
         updateClientView();
         initNetwork();
+
         clientView.setOnMouseClicked(e -> {
             String item = clientView.getSelectionModel().getSelectedItem();
             if (item.equals("...")) {
--- src/main/java/com/gb/server/CloudFileHandler.java
+++ src/main/java/com/gb/server/CloudFileHandler.java
@@ -2,6 +2,9 @@ package com.gb.server;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CloudFileHandler implements Runnable {
     private static final int BUFFER_SIZE = 8192;
@@ -18,6 +21,31 @@ public class CloudFileHandler implements Runnable {
         os = new DataOutputStream(socket.getOutputStream());
         buf = new byte[BUFFER_SIZE];
         serverDirectory = new File("server");
+        sendServerFiles();
+        sendCurrentDirName();
+    }
+
+    private List<String> getServerFiles() {
+        String[] names = serverDirectory.list();
+        if (names != null) {
+            return Arrays.asList(serverDirectory.list());
+        }
+        return new ArrayList<>();
+    }
+
+    private void sendCurrentDirName() throws IOException {
+        os.writeUTF("#path#");
+        os.writeUTF(serverDirectory.getName());
+    }
+
+    private void sendServerFiles() throws IOException {
+        //#list# size name1 name2 .... name_size
+        os.writeUTF("#list#");
+        List<String> files = getServerFiles();
+        os.writeInt(files.size());
+        for (String file : files) {
+            os.writeUTF(file);
+        }
     }
 
     @Override
@@ -37,8 +65,22 @@ public class CloudFileHandler implements Runnable {
                                 fos.write(buf, 0, readCount);
                             }
                         }
-                        System.out.println("File: " + name + " is uploaded");
-                    }else {
+                        System.out.println("File: " + name + " was uploaded");
+                        sendServerFiles();
+                    } else if ("#get_file#".equals(command)) {
+                        String fileName = is.readUTF();
+                        File file = serverDirectory.toPath().resolve(fileName).toFile();
+                        os.writeUTF("#file_message#");
+                        os.writeUTF(fileName);
+                        os.writeLong(file.length());
+                        try (InputStream fis = new FileInputStream(file)) {
+                            while (fis.available() > 0) {
+                                int count = fis.read(buf);
+                                os.write(buf, 0, count);
+                            }
+                        }
+                        os.flush();
+                    } else {
                         System.err.println("Unknown command" + command);
                     }
                 }
